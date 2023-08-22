cd11049dafe011b26f487de40993ed96f787445e
Huski <hugo.rissling@gmail.com>
2023-08-22 21:51:58 +0300

    ...

---
+++ Moduuli 2/Tehtävä 2-2.py
@@ -0,0 +1,4 @@
+import math
+r = input("Anna ympyrän säde (cm): ")
+A = math.pi*int(r)**2
+print("A =", A, "cm^2")
---
+++ Moduuli 2/Tehtävä 2-3.py
@@ -0,0 +1,6 @@
+a = int(input("Anna suorakulmion kanta (cm): "))
+b = int(input("Anna suorakulmion korkeus (cm): "))
+p = a*2 + b*2
+A = a*b
+print("p =", p, "cm")
+print("A =", A, "cm^2")
