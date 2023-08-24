1cdf7b8c0bd84686d9340b433d74d39b3f2aae31
HuskiRizzling <hugo.rissling@gmail.com>
2023-08-24 09:31:04 +0300

    t 2-4 lisätty

---
+++ Moduuli 2/Tehtävä 2-4.py
@@ -0,0 +1,9 @@
+a = int(input("Anna kokonaisluku: "))
+b = int(input("Anna toinen kokonaisluku: "))
+c = int(input("Anna kolmas kokonaisluku: "))
+Summa = a + b + c
+Tulo = a*b*c
+Keskiarvo = Summa / 3
+print("Summa on", Summa)
+print("Tulo on", Tulo)
+print("Keskiarvo on ", Keskiarvo)
