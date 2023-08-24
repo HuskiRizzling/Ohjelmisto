5937f72ffefbeed21c1afa7c25c529ddd0015269
HuskiRizzling <hugo.rissling@gmail.com>
2023-08-24 11:33:49 +0300

    toka moduuli done

---
+++ Moduuli 2/Tehtävä 2-5.py
@@ -0,0 +1,6 @@
+leiviska = int(input("Anna leiviskät: "))
+naula = int(input("Anna naulat: "))
+luoti = int(input("Anna luodit: "))
+mg = ((leiviska*20*32+naula*32+luoti)*13.3)%1000
+mk = ((leiviska*20*32+naula*32+luoti)*13.3)/1000
+print(f"{mk:.0f} kiloa ja", f"{mg:.2f} grammaa")
---
+++ Moduuli 2/Tehtävä 2-6.py
@@ -0,0 +1,7 @@
+import random
+print("kolme numeroinen koodi: ")
+for i in range(3):
+    print(random.randint(0,9))
+print("\n" "neljä numeroinen koodi: ")
+for i in range(4):
+    print(random.randint(0,6))
