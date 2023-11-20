4b41c0b2003a9349623015ff67684c11cfae8b63
Huski <hugo.rissling@gmail.com>
2023-11-20 22:36:07 +0200

    homoneek

--- Ohjelmisto1+2/Moduuli 13/Tehtävä 13-1.py
+++ Ohjelmisto1+2/Moduuli 13/Tehtävä 13-1.py
@@ -1,15 +1,21 @@
 from flask import Flask, request
 
 app = Flask(__name__)
+
+
 @app.route('/alkuluku/<number>')
 def prime(number):
     isprime = True
     kl = int(number)
-    for i in range(2, kl):
-        if kl % i == 0 and (i != 1 or i != kl):
-            isprime = False
-            break
+    if kl == 1:
+        isprime = False
+    elif kl > 1:
+        for i in range(2, kl):
+            if kl % i == 0:
+                isprime = False
+                break
     return {"Number": number, "isPrime": isprime}
+
 
 if __name__ == '__main__':
     app.run(use_reloader=True, host='127.0.0.1', port=3000)
