8504924332c5bdf6567bcc246c1238909242bd14
Huski <hugo.rissling@gmail.com>
2023-11-20 23:27:39 +0200

    homoneek

---
+++ java/Moduuli 3/2/2.js
@@ -0,0 +1,9 @@
+const targetElement = document.getElementById('target');
+
+const items = [
+    "First item", "Second item", "Third item"
+]
+const i = document.createElement("li");
+
+p = document.getElementById("target")
+p.appendChild(i)
